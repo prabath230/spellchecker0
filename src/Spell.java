75ee856200295a8f407045a68d9357e22b9edbd2
Your Name <you@example.com>
2019-01-14 13:25:26 +0530

    1 step creating database

--- src/Spell.java
+++ src/Spell.java
@@ -8,6 +8,7 @@
 
 
 import java.sql.*;
+import java.util.ArrayList;
 
 
 /**
@@ -16,13 +17,50 @@ import java.sql.*;
  */
 public class Spell {
     
+ public static Connection getConnection() throws Exception{
+  try{
+   String driver = "com.mysql.jdbc.Driver";
+   String url = "jdbc:mysql://localhost:3306/dictionary?zeroDateTimeBehavior=convertToNull";
+   String username = "root";
+   String password = "";
+   Class.forName(driver);
+   
+   Connection conn = DriverManager.getConnection(url,username,password);
+   System.out.println("Connected");
+   return conn;
+  } catch(Exception e){System.out.println(e);}
+  
+  
+  return null;
+ }
 
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         
-    dbconnect connet = new dbconnect();
-         connet.getdata();
+      getConnection();
+      get();
 }
+    
+    public static ArrayList<String> get() throws Exception{
+        try{
+            Connection con = getConnection();
+            PreparedStatement statement = con.prepareStatement("SELECT * FROM mydic");
+           
+            ResultSet result = statement.executeQuery();
+           
+            ArrayList<String> array = new ArrayList<String>();
+            while(result.next()){
+                System.out.print(result.getString("Word"));
+                System.out.print(" ");
+                System.out.println(result.getString("Count"));
+             
+            }
+            System.out.println("All records have been selected!");
+            return array;
+           
+        }catch(Exception e){System.out.println(e);}
+        return null;
+       
+    }
     };
     
